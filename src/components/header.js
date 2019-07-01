c0f2838284dfc74e7f3339bfa654315ade630a64
Trevor Fehrman <trevorfehrman@gmail.com>
2019-07-01 16:23:59 -0700

    setup

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+import { Link } from 'gatsby';
+
+const NavLink = styled(Link)`
+  color: #222;
+  font-size: 1rem;
+  font-weight: normal;
+  line-height: 1;
+`;
+
+const Header = () => (
+  <>
+    <header></header>
+  </>
+);
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,9 @@ import Layout from '../components/layout';
 
 export default () => (
   <Layout>
-    <h1>Home</h1>
-    <p>Stuff</p>
-    <Link to="about">&rarr; About</Link>
+    <h1>About Me</h1>
+
+    <p>This is my personal website</p>
+    <Link to="/">&larr; Back to Home</Link>
   </Layout>
 );

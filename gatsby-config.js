a1c53deb07ec0307414c2f20b724493df42001aa
Trevor Fehrman <trevorfehrman@gmail.com>
2019-07-01 21:49:03 -0700

    lots of stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Frontend Gatsby Workshop',
+    description: 'A site we built together',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Link } from 'gatsby';
+// import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
+import ReadLink from '../components/read-link';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Stuff</p>
-    <Link to="about">&rarr; About</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Stuff</p>
+      <ReadLink to="about">&rarr; About</ReadLink>
+
+      <h2>My Blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
